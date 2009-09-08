da6f27ede6515599614d9970d51bbebcb9787efb
Thomas Bruyelle <thomas.bruyelle@gmail.com>
2009-09-08 22:37:29 +0000

    nc

--- src/net/tsoft/resetunreadsms/ResetUnreadSms.java
+++ src/net/tsoft/resetunreadsms/ResetUnreadSms.java
@@ -1,5 +1,6 @@
-package net.tom.resetunreadsms;
+package net.tsoft.resetunreadsms;
 
+import net.tsoft.resetunreadsms.R;
 import android.app.ListActivity;
 import android.app.ProgressDialog;
 import android.content.ContentValues;
@@ -31,6 +32,7 @@ public class ResetUnreadSms
   public void onCreate(Bundle savedInstanceState)
   {
     super.onCreate(savedInstanceState);
+    setTitle(getText(R.string.unreadsms_label));
     setContentView(R.layout.main);
     resetButton = (Button) findViewById(R.id.reset_button);
     resetButton.setOnClickListener(new View.OnClickListener()
@@ -48,18 +50,20 @@ public class ResetUnreadSms
   private void resetUnreadSms()
   {
     ProgressDialog progressDialog = ProgressDialog.show(this, "", this
-        .getString(R.string.updating_sms));
+        .getString(R.string.updating_sms), true);
+    progressDialog.show();
     ContentValues values = new ContentValues(1);
     values.put(READ, 1);
     this.getContentResolver().update(Uri.parse(SMS_CONTENT_URI), values, null, null);
-    Toast.makeText(this, R.string.reset_done, Toast.LENGTH_SHORT);
+    Toast.makeText(this, R.string.reset_done, Toast.LENGTH_LONG).show();
     progressDialog.hide();
   }
 
   private void fillList()
   {
     ProgressDialog progressDialog = ProgressDialog.show(this, "", this
-        .getString(R.string.loading_sms));
+        .getString(R.string.loading_sms), true);
+    progressDialog.show();
     Cursor c = this.getContentResolver().query(Uri.parse(SMS_CONTENT_URI),
         new String[] { SMS_ID, SMS_BODY }, READ_CONDITION, null, null);
     resetButton.setEnabled(c.getCount() != 0);
